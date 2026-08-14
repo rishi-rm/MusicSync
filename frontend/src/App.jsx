47786bac4f3bb297b38e0ddd04046d87a46edebf
rishi-rm <rishabh124m@gmail.com>
2026-08-14 00:18:36 -0700

    shit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -106,7 +106,7 @@ export default function App() {
                 onClick={async () => {
                     if (songName.trim() && artistName.trim()) setLoading(true);
 
-                    await fetch("http://localhost:8000/api/download", {
+                    await fetch("http://localhost:8001/api/download", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json",
